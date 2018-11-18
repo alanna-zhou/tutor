6f40f3b90afe2136762ddb100b4743a9a3b2bb2e
asz33@cornell.edu <asz33@cornell.edu>
2018-11-18 17:17:50 -0500

    Start getting all courses with course api

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import json
+import requests
 from db import db, User
 from flask import Flask, request
 
@@ -26,6 +27,16 @@ def create_user():
   db.session.commit()
   return json.dumps({'success': True, 'data': user.serialize()}), 201
 
+@app.route('/api/courses/', methods=['GET'])
+def get_all_courses():
+  r = requests.get('https://classes.cornell.edu/api/2.0/search/classes.json?roster=SP19&subject=CS')
+  jsonfile = r.json()
+  data = jsonfile.get('data', '')
+  classes = data.get('classes', '')
+  list = []
+  for c in classes:
+    list.append({'subject': c.get('subject', ''), 'number': c.get('catalogNbr')})
+  return json.dumps(list)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
