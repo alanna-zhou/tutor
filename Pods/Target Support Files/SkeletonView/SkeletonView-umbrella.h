54b438e020aec8729f01eb3f7d1ef083a4d440fa
zeliboid8 <zeliboid8@gmail.com>
2018-11-27 15:51:27 -0500

    Changing pod files around

--- Pods/Target Support Files/SkeletonView/SkeletonView-umbrella.h
+++ Pods/Target Support Files/SkeletonView/SkeletonView-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double DropDownVersionNumber;
-FOUNDATION_EXPORT const unsigned char DropDownVersionString[];
+FOUNDATION_EXPORT double SkeletonViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char SkeletonViewVersionString[];
 
